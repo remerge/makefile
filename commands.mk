e15053697960e5addc7f3d0160d50656b67dddd0
Benedikt Böhm <bb@xnull.de>
2022-04-01 22:29:53 +0200

    add brew bundle install

--- commands.mk
+++ commands.mk
@@ -1,9 +1,9 @@
 has-command = $(if $(shell which $(1)),$(1),$(2))
 
 need-command = $(if $(call has-command,$(1)),$(1),\
-	$(call log,error,"missing $(1) in PATH (consider brew install $(1))"))
+	$(call log,error,"missing $(1) in PATH (run make install)"))
 
 want-command = $(if $(call has-command,$(1)),$(1),\
-	$(call log,warning,"no $(1) in PATH (consider brew install $(1))"))
+	$(call log,warning,"no $(1) in PATH (run make install)"))
 
 first-command = $(firstword $(foreach command,$(1),$(call want-command,$(1))))
